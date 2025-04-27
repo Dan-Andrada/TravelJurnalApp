e948e51ba16576f7dcd1c8853779b6c7f536ef9d
Andrada <elena.dan@student.upt.ro>
2025-04-27 18:27:50 +0300

    Improved login and register, implemented 'Forgot password'

--- app/src/main/java/com/example/traveljurnalapp/RegisterActivity.java
+++ app/src/main/java/com/example/traveljurnalapp/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.traveljurnalapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -62,6 +63,23 @@ public class RegisterActivity extends AppCompatActivity {
             return;
         }
 
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Invalid email format", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (password.length() < 6) {
+            Toast.makeText(this, "Password must be at least 6 characters long", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String passwordPattern = "(.*[A-Z].*)(.*[0-9].*)";
+
+        if (!password.matches(passwordPattern)) {
+            Toast.makeText(this, "Password must contain at least one uppercase letter and one number", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         if (!password.equals(confirmPassword)) {
             Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
             return;
@@ -85,9 +103,9 @@ public class RegisterActivity extends AppCompatActivity {
                                     startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                                     finish();
                                 })
-                                .addOnFailureListener(e ->
-                                        Toast.makeText(this, "Error saving data: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+                                .addOnFailureListener(e -> {
                                         user.delete();
+                                        Toast.makeText(this, "Error saving data: " + e.getMessage(), Toast.LENGTH_SHORT).show(); });
                     } else {
                         Toast.makeText(this, "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
