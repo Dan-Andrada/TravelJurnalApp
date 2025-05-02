1793b742117dc2339f00305a648293ae5ed7f61b
Clipici Maria-Gabriela <101906050+clipici-gabriela@users.noreply.github.com>
2025-05-02 13:42:09 +0300

    feature add for trip details

---
+++ app/src/main/java/com/example/traveljurnalapp/AddTripDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.traveljurnalapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddTripDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trip_details);
+    }
+}
